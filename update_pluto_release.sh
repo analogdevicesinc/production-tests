ad39758b7222710102c3e2dfb2e13ebb6d38b72c
Michael Hennerich <michael.hennerich@analog.com>
2018-04-05 15:15:32 +0200

    update_pluto|m2k_release.sh: Get latest version if no version is provided
    Be aware this changes the parameter exptected from x.xx to vx.xx
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- update_pluto_release.sh
+++ update_pluto_release.sh
@@ -2,6 +2,12 @@
 
 echo_red()   { printf "\033[1;31m$*\033[m\n"; }
 
+get_latest_release() {
+	curl --silent "https://api.github.com/repos/$1/releases/latest" |
+	grep '"tag_name":' |
+	sed -E 's/.*"([^"]+)".*/\1/'
+}
+
 #----------------------------------#
 # Main section                     #
 #----------------------------------#
@@ -9,8 +15,8 @@ echo_red()   { printf "\033[1;31m$*\033[m\n"; }
 VERSION_TO_UPDATE="$1"
 
 [ -n "$VERSION_TO_UPDATE" ] || {
-	echo_red "No version provided for pluto release"
-	exit 1
+	VERSION_TO_UPDATE=$(get_latest_release analogdevicesinc/plutosdr-fw)
+	echo_red "No version provided for pluto release getting latest $VERSION_TO_UPDATE"
 }
 
 RELEASE_DIR="$(pwd)/release/pluto"
@@ -22,7 +28,7 @@ RELEASE_DIR="$(pwd)/release/pluto"
 	fi
 }
 
-FW_URL="https://github.com/analogdevicesinc/plutosdr-fw/releases/download/v${VERSION_TO_UPDATE}/plutosdr-fw-v${VERSION_TO_UPDATE}.zip"
-FW_BOOTSTRAP_URL="https://github.com/analogdevicesinc/plutosdr-fw/releases/download/v${VERSION_TO_UPDATE}/plutosdr-jtag-bootstrap-v${VERSION_TO_UPDATE}.zip"
+FW_URL="https://github.com/analogdevicesinc/plutosdr-fw/releases/download/${VERSION_TO_UPDATE}/plutosdr-fw-${VERSION_TO_UPDATE}.zip"
+FW_BOOTSTRAP_URL="https://github.com/analogdevicesinc/plutosdr-fw/releases/download/${VERSION_TO_UPDATE}/plutosdr-jtag-bootstrap-${VERSION_TO_UPDATE}.zip"
 
 ./common_update_release.sh "$RELEASE_DIR" "pluto.dfu" "$FW_URL" "$FW_BOOTSTRAP_URL"
