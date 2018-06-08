0d13e51e33c2dee433e4b310921f7fe39205d1a4
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-06-08 11:36:35 +0300

    config/pluto/postflash.sh: suppress iio_attr stderr output
    It's intermediate output/failure that could just confuse some users.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- config/pluto/postflash.sh
+++ config/pluto/postflash.sh
@@ -13,7 +13,7 @@ source config.sh
 wait_for_board() {
 	local serial
 	for iter in $(seq $BOARD_ONLINE_TIMEOUT) ; do
-		serial=$(iio_attr -C $IIO_URI_MODE hw_serial | cut -d ' ' -f2)
+		serial=$(iio_attr -C $IIO_URI_MODE hw_serial 2> /dev/null | cut -d ' ' -f2)
 		[ -z "$serial" ] || return 0
 		sleep 1
 	done
