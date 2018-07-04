44c3088c97da2ab173b716966d790247f888df15
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-07-04 10:43:38 +0300

    production_m2k.sh: add m2k production script
    Re-use the skeleton parts from lib/production.sh
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ production_m2k.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wrapper script for doing a production cycle/routine for a M2k board.
+# This script handles 
+#  * ADC measurements and validation of voltages
+#  * Flashing of the board
+#  * Post-flash - AD9361 calibration, measurements and Linux system validation
+#
+# Can be called with:  ./production_m2k.sh
+#
+# The ./update_m2k_release.sh must be called to update release files
+
+./lib/production.sh "m2k"
