5736ebd4244626c2bb8cc339930f1fd030d857ee
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-07-18 16:19:56 +0300

    lib/preflash.sh: retry measurements, not toggling
    We should retry the measurements of the values to check whether they're in
    range. We shouldn't toggle the GPIOs, since they should be at the desired
    state already.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- lib/preflash.sh
+++ lib/preflash.sh
@@ -86,30 +86,33 @@ validate_range_values() {
 }
 
 power_off_and_measure() {
+	local retries="${1:-4}"
 	echo_green "   Measuring voltages with board off"
 	disable_all_usb_ports
 	power_cycle_sleep
-	check_voltage_ranges "BOARD_OFF" || return 1
+	retry "$retries" check_voltage_ranges "BOARD_OFF" || return 1
 	echo_green "   .Done - values are within range"
 }
 
 power_on_usb_1_and_measure() {
+	local retries="${1:-4}"
 	echo_green "   Measuring voltages with USB port 1 enabled"
 	disable_all_usb_ports
 	power_cycle_sleep
 	enable_usb_port_1
 	power_cycle_sleep
-	check_voltage_ranges "BOARD_ON" || return 1
+	retry "$retries" check_voltage_ranges "BOARD_ON" || return 1
 	echo_green "   .Done - values are within range"
 }
 
 power_on_usb_2_and_measure() {
+	local retries="${1:-4}"
 	echo_green "   Measuring voltages with USB port 2 enabled"
 	disable_all_usb_ports
 	power_cycle_sleep
 	enable_usb_port_2
 	power_cycle_sleep
-	check_voltage_ranges "BOARD_ON" || return 1
+	retry "$retries" check_voltage_ranges "BOARD_ON" || return 1
 	echo_green "   .Done - values are within range"
 }
 
@@ -135,9 +138,9 @@ pre_flash() {
 
 	for measure_cnt in $(seq 1 $MEASUREMENT_CYCLES); do
 		echo_blue "Running measurement cycle $measure_cnt"
-		retry 4 power_off_and_measure || return 1
-		retry 4 power_on_usb_1_and_measure || return 1
-		retry 4 power_on_usb_2_and_measure || return 1
+		power_off_and_measure 10 || return 1
+		power_on_usb_1_and_measure 10 || return 1
+		power_on_usb_2_and_measure 10 || return 1
 	done
 
 	return 0
