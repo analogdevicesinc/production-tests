e6a2faa5a71ded24b01e0a09ae0588e9dd77a0ad
Bogdan Togorean <bogdan.togorean@analog.com>
2020-01-27 17:02:26 +0200

    Add FMCOMMS8 FRU EEPROM write
    FMCOMMS8 on board EEPROM must be written at the end of testing procedure.
    
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- adrv_fmcomms8_test/fmcomms8_test.sh
+++ adrv_fmcomms8_test/fmcomms8_test.sh
@@ -6,4 +6,10 @@ source $SCRIPT_DIR/test_powersupply.sh
 answer=$?
 proceed_if_ok $answer
 
+TIMED_LOG_SUFFIX="FRU"
+
+timed_log "Setting FMCOMMS8 FRU EEPROM data"
+source $SCRIPT_DIR/fru_write.sh
+write_fru
+
 echo
