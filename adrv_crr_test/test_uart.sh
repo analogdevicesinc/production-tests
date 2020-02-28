a9ddef6aeaf69b074cb9b5acd26deb194909d45c
Bogdan Togorean <bogdan.togorean@analog.com>
2020-02-28 13:49:05 +0200

    Add UART test
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

---
+++ adrv_crr_test/test_uart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(readlink -f $(dirname $0))"
+
+source $SCRIPT_DIR/test_util.sh
+
+TEST_NAME="TEST_UART_COMM"
+
+TEST_ID="01"
+SHORT_DESC="Connect MicroUSB cable to carrier UART. Check if USB-UART is detected"
+CMD="wait_enter && test -e /dev/ttyUSB0"
+run_test $TEST_ID "$SHORT_DESC" "$CMD"
+
+TEST_ID="02"
+SHORT_DESC="Check UART communication"
+CMD="stty -F /dev/ttyUSB0 115200;"
+CMD+="echo \"cat /proc/device-tree/model\" > /dev/ttyUSB0;"
+CMD+="cat /dev/ttyUSB0;"
+run_test $TEST_ID "$SHORT_DESC" "$CMD"
+
+ : #if reached this point, ensure exito code 0
