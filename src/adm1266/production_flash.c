fa312550f7a24b0f6456cb2bbf97241e410dcc10
Bogdan Togorean <bogdan.togorean@analog.com>
2020-04-01 10:31:12 +0300

    adm1266: Add new line after every print for instant flush when exec is run from shell
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- src/adm1266/production_flash.c
+++ src/adm1266/production_flash.c
@@ -45,13 +45,23 @@ int main(int argc, char *argv[])
 	//ADM1266_ptr_file_fw = fopen("./config_files/adm1266_v1.14.2.hex", "r");
 	ADM1266_ptr_file_fw = fopen("./config_files/adm1266_v1.14.3.hex", "r");
 
+	if(!ADM1266_ptr_file_fw) {
+		printf("Input file \"adm1266_v1.14.3.hex\" could not be open!\n");
+		exit(1);
+	}
+
 	// Path of the configuration file for linux
 	ADM1266_ptr_file_cfg[0] = fopen("./config_files/TaliseSOM_Sequencing_ADM1266@48.hex", "r");
+	
+	if(!ADM1266_ptr_file_cfg[0]) {
+		printf("Input file \"TaliseSOM_Sequencing_ADM1266@48.hex\" could not be open!\n");
+		exit(1);
+	}
 
 	// Check if all the devices are present
 	if (ADM1266_Device_Present(ADM1266_Address, ADM1266_NUM) == 0)
 	{
-		printf("ADM1266 Power Sequencer not detected. Please check connection");
+		printf("ADM1266 Power Sequencer not detected. Please check connection\n");
 		return -1;
 	}
 	else
