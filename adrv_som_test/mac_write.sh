6bb94cced411311939278204457720bf431dbe50
Bogdan Togorean <bogdan.togorean@analog.com>
2020-04-07 16:41:31 +0300

    mac_write: Add check of MAC address format and write only SOM ETH0 MAC
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- adrv_som_test/mac_write.sh
+++ adrv_som_test/mac_write.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 QSPI_ENV_PART="/dev/mtd1"
 QSPI_ENV_PART_SIZE="20000"
 MKENVIMAGE_PATH="/usr/bin/mkenvimage"
-MAC_PREFIX="aa:bb:cc:dd:"
+MAC_PREFIX="00:e0:22:fe:"
 
 
 source $SCRIPT_DIR/test_util.sh
@@ -26,13 +26,23 @@ function check_req() {
 
 function write_mac() {
 	check_req
-	read -p 'Please enter last four digits of ETH0 mac address (e.g. aa:bb): ' MAC_ETH0
-	MAC_ETH0="${MAC_PREFIX}${MAC_ETH0}";
-	read -p 'Please enter last four digits of ETH1 mac address (e.g. aa:bb): ' MAC_ETH1
-	MAC_ETH1="${MAC_PREFIX}${MAC_ETH1}";
 
-	echo -e "ethaddr=$MAC_ETH0\n" > /tmp/uboot-env.txt
-	echo -e "eth1addr=$MAC_ETH1\n" >> /tmp/uboot-env.txt
+	local format_ok=false
+
+	until [ $format_ok = true ]
+	do
+		read -p 'Please enter last four digits of ETH0 mac address (e.g. aa:bb): ' MAC_ETH0
+		if [[ $MAC_ETH0 =~ ^([0-9a-f]{2})(:[0-9a-f]{2})$ ]]; 
+			then 
+				format_ok=true
+			else
+				echo "Wrong Format: Should be four hex digits in groups of two separated by :"
+			fi
+	done
+
+	MAC_ETH0="${MAC_PREFIX}${MAC_ETH0}";
+
+	echo "ethaddr=$MAC_ETH0" > /tmp/uboot-env.txt
 
 	mkenvimage -s 0x20000 -o /tmp/uboot-env.bin /tmp/uboot-env.txt
 
