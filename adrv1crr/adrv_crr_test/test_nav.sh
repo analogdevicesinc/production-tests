59e6327e909a13a2de0757399cbc18129815b5d5
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2021-11-12 12:45:15 +0200

    test_prod: adrv1crr: update nav test
    Nav test checking for appropriate results from
    buttons, switches and leds.
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- adrv1crr/adrv_crr_test/test_nav.sh
+++ adrv1crr/adrv_crr_test/test_nav.sh
@@ -32,12 +32,12 @@ run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
 TEST_ID="06"
 SHORT_DESC="toggle SW0 - timeout 10 seconds" 
-CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 3 (SW_RFKILL_ALL), value 0\" > /dev/null"
+CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 1 (SW_TABLET_MODE), value 0\" > /dev/null"
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
 TEST_ID="07"
 SHORT_DESC="toggle SW1 - timeout 10 seconds" 
-CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 1 (SW_TABLET_MODE), value 0\" > /dev/null"
+CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 3 (SW_RFKILL_ALL), value 0\" > /dev/null"
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
 TEST_ID="08"
@@ -47,19 +47,19 @@ run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
 TEST_ID="09"
 SHORT_DESC="toggle SW3 - timeout 10 seconds" 
-CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 3 (SW_RFKILL_ALL), value 0\" > /dev/null"
+CMD="timeout 10 evtest /dev/input/by-path/platform-gpio_keys-event | grep -m 1 \"type 5 (EV_SW), code 13 (SW_LINEIN_INSERT), value 0\" > /dev/null"
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
 TEST_ID="10"
 SHORT_DESC="test LED0-3" 
-CMD="echo heartbeat | tee /sys/class/leds/led0:green/trigger > /dev/null;"
-CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led1:green/trigger > /dev/null;"
-CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led2:green/trigger > /dev/null;"
-CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led3:green/trigger > /dev/null;"
+CMD="echo heartbeat | tee /sys/class/leds/led0:red/trigger > /dev/null;"
+CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led1:red/trigger > /dev/null;"
+CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led2:red/trigger > /dev/null;"
+CMD+="sleep 0.1 && echo heartbeat | tee /sys/class/leds/led3:red/trigger > /dev/null;"
 CMD+="YES_no 'Are LEDs 0-3 blinking ? '"
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
-echo none | tee /sys/class/leds/led[0-3]\:green/trigger > /dev/null
+echo none | tee /sys/class/leds/led[0-3]\:red/trigger > /dev/null
 
 : #if reached this point, ensure exit code 0
 
