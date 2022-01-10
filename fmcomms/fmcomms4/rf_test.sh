9b1d285f739fa68f46d8b1e9e10fb42ed2ef100b
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-01-10 10:15:35 +0200

    prod_tests: fmcomms4: beautify and enforce order of execution
    Do not let user run the rf_tests without calibrating first
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- fmcomms/fmcomms4/rf_test.sh
+++ fmcomms/fmcomms4/rf_test.sh
@@ -1,5 +1,13 @@
 ANSWER=1
 
+ssh_cmd "fru-dump -i /sys/devices/soc0/fpga-axi@0/41600000.i2c/i2c-0/i2c-7/7-0050/eeprom -b | grep 'Tuning' | cut -d' ' -f3 | tr -d '[:cntrl:]'"
+CALIB_DONE=$?
+
+if [ CALIB_DONE -ne 0 ]; then
+    printf "\033[1;31mPlease run calibration first\033[m\n"
+    exit 1
+fi
+
 echo
 python3 -m pytest --color yes -vs /home/analog/pyadi-iio/test/test_fmcomms4_prod.py --uri="ip:127.0.0.1" --adi-hw-map --hw=fmcomms4
 ANSWER=$?
