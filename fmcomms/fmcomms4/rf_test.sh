3eeadc9e60ea8a0683d92192ca1a04a64d31ffb1
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-01-10 12:30:19 +0200

    prod_test: fmcomms4: small fix in rf_tests
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- fmcomms/fmcomms4/rf_test.sh
+++ fmcomms/fmcomms4/rf_test.sh
@@ -3,7 +3,7 @@ ANSWER=1
 sudo fru-dump -i /sys/devices/soc0/fpga-axi@0/41600000.i2c/i2c-0/i2c-7/7-0050/eeprom -b | grep 'Tuning' | cut -d' ' -f4 | tr -d '[:cntrl:]'
 CALIB_DONE=$?
 
-if [ CALIB_DONE -ne 0 ]; then
+if [ $CALIB_DONE -ne 0 ]; then
     printf "\033[1;31mPlease run calibration first\033[m\n"
     exit 1
 fi
