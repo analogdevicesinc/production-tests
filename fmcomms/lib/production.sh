fa4fa459551e756326fb04b163c231653c7425c4
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-01-11 14:12:07 +0200

    prod_test: fmcomms4: check for tuning before rf_test
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- fmcomms/lib/production.sh
+++ fmcomms/lib/production.sh
@@ -212,6 +212,13 @@ production() {
 
         case $MODE in
                 "FMCOMMS4 Test")
+						ssh_cmd "sudo fru-dump -i /sys/devices/soc0/fpga-axi@0/41600000.i2c/i2c-0/i2c-7/7-0050/eeprom -b | grep 'Tuning' | cut -d' ' -f4 | tr -d '[:cntrl:]'"
+						CALIB_DONE=$?
+
+						if [ $CALIB_DONE -ne 0 ]; then
+							printf "\033[1;31mPlease run calibration first\033[m\n"
+							handle_error_state "$BOARD_SERIAL"
+						fi
                         $SCRIPT_DIR/fmcomms4/rf_test.sh
                         if [ $? -ne 0 ]; then
                                 handle_error_state "$BOARD_SERIAL"
