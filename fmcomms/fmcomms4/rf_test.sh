9a09347d596a36050ec65772f3e836f105912d61
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-01-11 14:12:07 +0200

    prod_test: fmcomms4: check for tuning before rf_test
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- fmcomms/fmcomms4/rf_test.sh
+++ fmcomms/fmcomms4/rf_test.sh
@@ -2,14 +2,6 @@ ANSWER=1
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
-sudo fru-dump -i /sys/devices/soc0/fpga-axi@0/41600000.i2c/i2c-0/i2c-7/7-0050/eeprom -b | grep 'Tuning' | cut -d' ' -f4 | tr -d '[:cntrl:]'
-CALIB_DONE=$?
-
-if [ $CALIB_DONE -ne 0 ]; then
-    printf "\033[1;31mPlease run calibration first\033[m\n"
-    exit 1
-fi
-
 echo
 python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_fmcomms4_prod.py --uri="ip:analog.local" --adi-hw-map --hw=fmcomms4
 ANSWER=$?
