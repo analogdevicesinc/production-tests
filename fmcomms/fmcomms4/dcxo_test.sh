4e6980dfef54dab831c340338f284071c35a42ee
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-01-11 12:42:07 +0200

    prod_tests: fmcomms4: fix path for pytest
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- fmcomms/fmcomms4/dcxo_test.sh
+++ fmcomms/fmcomms4/dcxo_test.sh
@@ -1,7 +1,8 @@
 
+SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
 echo
-python3 -m pytest --color yes -vs $SCRIPT_DIR/work/pyadi-iio/test/test_dcxo.py --uri="ip:analog.local" --adi-hw-map --hw=fmcomms4 | tee calib.txt
+python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_dcxo.py --uri="ip:analog.local" --adi-hw-map --hw=fmcomms4 | tee calib.txt
 
 cat calib.txt | grep "1 passed" &>/dev/null
 result=$?
