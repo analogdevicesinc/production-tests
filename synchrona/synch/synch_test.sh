f11ceeae8c70a65df8fd517609518412cea70408
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-02-14 12:35:39 +0200

    prod: synchrona: add synchrona general script
    Added one script to call all the other
    which run on synchrona.
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

---
+++ synchrona/synch/synch_test.sh
@@ -0,0 +1,31 @@
+
+SCRIPT_DIR="$(readlink -f $(dirname $0))"
+
+source $SCRIPT_DIR/clk_disable.sh
+answer=$?
+proceed_if_ok $answer
+
+echo
+source $SCRIPT_DIR/clk_source.sh
+answer=$?
+proceed_if_ok $answer
+
+echo
+source $SCRIPT_DIR/test_i2c_temp.sh
+answer=$?
+proceed_if_ok $answer
+
+# echo
+# source $SCRIPT_DIR/test_clk.sh
+# answer=$?
+# proceed_if_ok $answer
+
+# echo
+# source $SCRIPT_DIR/test_eth.sh
+# answer=$?
+# proceed_if_ok $answer
+
+# echo
+# source $SCRIPT_DIR/test_usb.sh
+# answer=$?
+# proceed_if_ok $answer
