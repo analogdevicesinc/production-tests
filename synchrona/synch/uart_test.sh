5f5aca255432645eafee1446e6dad907d9e169c8
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-02-14 11:10:00 +0200

    prod: synchrona: add misc tests and production script
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- synchrona/synch/uart_test.sh
+++ synchrona/synch/uart_test.sh
@@ -1,12 +1,12 @@
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
 source $SCRIPT_DIR/test_util.sh
-# FAIL_COUNT=0
+FAIL_COUNT=0
 
 TEST_NAME="TEST_UART_COMM"
 
 TEST_ID="01"
-SHORT_DESC="Connect MicroUSB cable to carrier UART. Check if USB-UART is detected"
+SHORT_DESC="Connect MicroUSB cable to adapter's port. Check if USB-UART is detected"
 CMD="wait_enter && test -e /dev/ttyUSB0"
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
