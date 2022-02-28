7b2b2132cd2b942f11912771fe965dc96627859e
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-02-28 16:05:14 +0200

    prod: synch: add manufacturing date to the labels
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- synchrona/print/print_basic.sh
+++ synchrona/print/print_basic.sh
@@ -5,13 +5,14 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 populate_label_fields()
 {
     SERIAL=$1
+    DATE=$(date +"%Y-%m-%d" )
     MODEL="AD-SYNCHRONA14-EBZ"
     MAC1=$(ifconfig | grep ether | awk 'NR==1 {print $2}')
     MAC2=$(ifconfig | grep ether | awk 'NR==2 {print $2}')
 
     rm -rf /tmp/csvfile.csv #remove previous data
 
-    echo $MODEL,$SERIAL,$MAC1,$MAC2 > /tmp/csvfile.csv
+    echo $MODEL,$SERIAL,$MAC1,$MAC2,$DATE > /tmp/csvfile.csv
 }
 
 
@@ -21,6 +22,7 @@ print_label()
 
     glabels-3-batch -o /tmp/back.pdf -i /tmp/csvfile.csv $SCRIPT_DIR/print/synchrona_back.glabels
     cancel -a -x
-    lpr -PLabelWriter-450-Turbo /tmp/back.pdf
+    PRINTER=$(lpstat -t | grep "printer LabelWriter" | awk '{print $2}')
+    lpr -P$PRINTER /tmp/back.pdf
 }
 
