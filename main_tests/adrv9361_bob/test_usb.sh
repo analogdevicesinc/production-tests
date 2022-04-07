2eab33f1f0a33731704a0a31a8c4653bf2929620
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-04-07 16:40:31 +0300

    prod: adrv9361: increase wait time for otg
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- main_tests/adrv9361_bob/test_usb.sh
+++ main_tests/adrv9361_bob/test_usb.sh
@@ -11,7 +11,7 @@ SHORT_DESC="USB port testing - please plug OTG cable with a connected USB2 flash
 CMD="wait_enter ;"
 CMD+="sudo bash -c 'echo ci_hdrc.0 > /sys/bus/platform/drivers/ci_hdrc/unbind' ;"
 CMD+="sudo bash -c 'echo ci_hdrc.0 > /sys/bus/platform/drivers/ci_hdrc/bind' ;"
-CMD+="sleep 1;"
+CMD+="sleep 2;"
 CMD+="lsusb -t | grep -q \"Class=Mass Storage, Driver=usb-storage\""
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
@@ -26,7 +26,7 @@ TEST_NAME="TEST_USB_HOST_MODE_SPEED"
 
 TEST_ID="03"
 SHORT_DESC="USB port speed test. Should be greater than 90MB/s"
-CMD="sleep 1;"
+CMD="sleep 2;"
 CMD+="drive_path=\$(lsblk | grep 'sda1' | awk -F'part ' '{ print \$2 }');"
 CMD+="read_speed=\$(hdparm -T /dev/sda1 | grep \"MB/sec\" | cut -d '=' -f 2 | cut -d 'M' -f 1); echo \"Speed: \$read_speed\";"
 CMD+="(( \$(echo \"\$read_speed > 90\" | bc -l -q) ))"
