759fe1d45272f51d9275f260762d1a0243a4e2cf
Constandachi <Monica.Constandachi@analog.com>
2022-07-25 14:49:26 +0300

    Changed static ip to analog.local

--- main_tests/adrv9364_bob/dcxo_test.sh
+++ main_tests/adrv9364_bob/dcxo_test.sh
@@ -4,7 +4,7 @@ source $SCRIPT_DIR/test_util.sh
 
 TEST_ID="01"
 SHORT_DESC="Testing crystal frequency"
-CMD="python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_dcxo.py --uri=\"ip:192.168.0.112\" --adi-hw-map --hw=adrv9364 | tee calib.txt;"
+CMD="python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_dcxo.py --uri=\"ip:analog.local\" --adi-hw-map --hw=adrv9364 | tee calib.txt;"
 CMD+="cat calib.txt | grep \"1 passed\""
 run_test $TEST_ID "$SHORT_DESC" "$CMD"
 
