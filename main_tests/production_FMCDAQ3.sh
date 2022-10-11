ad1dfd9ff3b7f4603b0b30f0fecd928770ca513f
Constandachi <Monica.Constandachi@analog.com>
2022-10-11 16:15:52 +0300

    Created test file for FMCDAQ3

---
+++ main_tests/production_FMCDAQ3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Wrapper script for doing a production cycle/routine for a rfsom-box.
+# This script handles
+#
+# Can be called with:  ./production_rfsom-box.sh
+#
+
+SCRIPT_DIR="$(readlink -f $(dirname $0))"
+ScriptLoc="$(readlink -f "$0")"
+
+source $SCRIPT_DIR/lib/production.sh
+source $SCRIPT_DIR/lib/utils.sh
+
+while true; do
+	echo_blue "Please enter your choice: "
+	options=("DCXO Calibration Test" "AD-FMCDAQ3 Test" "Power-Off Pi" "Power-Off Carrier")
+	select opt in "${options[@]}"; do
+    		case $REPLY in
+			1)
+				wait_for_board_online
+				get_board_serial
+				echo_blue "Starting FMCDAQ3 Calibration Test"
+				production "crr" "$opt" "FMCDAQ3"
+				break ;;
+			2)
+				wait_for_board_online
+				get_board_serial
+				echo_blue "Starting FMCDAQ3 Test"
+				production "crr" "$opt" "FMCDAQ3"
+				break ;;
+			3)
+				enforce_root
+				poweroff
+				break 2 ;;
+			4)
+				wait_for_board_online
+				ssh_cmd "sudo poweroff &>/dev/null"
+				break ;;
+			*) echo "invalid option $REPLY";;
+    		esac
+	done
+done
