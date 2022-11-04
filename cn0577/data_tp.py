e9e1778617421bc48c5ebfd38e7568ed9230db49
Richmond <richmondeustacio@gmail.com>
2022-11-04 20:24:48 +0800

    included fft plot

--- cn0577/data_tp.py
+++ cn0577/data_tp.py
@@ -31,13 +31,13 @@ sn = input("B#: ")
 # Prompt the user to connect an ADALM2000 test jig to analog inputs.
 input( " Connect ADALM2000 test jig ")
 
-ampl=2.048
-offset=0
+ampl= 2.048
+offset=2.048
 sine_gen.main(ampl, offset)
 print("ADALM2000 runs successfully")
 
-for n in range(5):
-    print("B",sn,"_",n+1)
+for n in range(1):
+    # print("B",sn,"_",n+1)
     # capture a block of 8192 (actually let's bump this to 256k, 2**18 )samples per channel -is this sampling freq or buffer size?
     my_adc = adi.ltc2387(uri=my_uri)
     # my_adc.rx_buffer_size = 131072
@@ -51,12 +51,22 @@ for n in range(5):
         if data[i] > (2 ** 17)-1:
             data[i] -= 2 ** 18
 
+    x2 = np.arange(0, len(data))
+    plt.figure(1)
+    plt.clf()
+    plt.plot(x2, data)
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("ADC data")
+    plt.show()
+    
+
     # Verify DC offset (average of all samples) less than TBD
     x = np.arange(0, len(data))
     voltage = data * 2.0 * vref / (2 ** 18)
     dc = np.average(voltage)  # Extract DC component
-    # plt.plot(voltage)
-    # plt.show()
+    plt.plot(voltage)
+    plt.title("Voltage reading")
+    plt.show()
     print("DC component=", dc)
 
     # Subtract DC offset from data record, apply window (what type of window?)
@@ -64,7 +74,8 @@ for n in range(5):
     print("AC component=", ac)
 
     # Record ADC code data
-    filename = str(sn)+ "_" + str(n+1) +"_ampl_" + str(ampl) +"_offset_" + str(offset) +".csv"
+    # filename = str(sn)+ "_" + str(n+1) +"_ampl_" + str(ampl) +"_offset_" + str(offset) +".csv"
+    filename = str(sn) +"_ampl_" + str(ampl) +"_offset_" + str(offset) +".csv"
     data_record = open(filename,"w")
     data_record.write("Data point,ADC data\n")
 
@@ -75,6 +86,20 @@ for n in range(5):
     # Take FFT of data (via sin_params.py functions), verify:
     # window_type= BLACKMAN_HARRIS_92
     fft_data = sin_params.windowed_fft_mag(voltage)
+    plt.plot(fft_data)
+    plt.title("FFT")
+    plt.xlabel("FFT Bin")
+    plt.ylabel("Amplitude")
+    # plt.ylim(-2, 2.5)
+    plt.show()
+
+    # filename1 = str(sn) +"_FFT_data" +".csv"
+    # data_record = open(filename1,"w")
+    # data_record.write("Data point,ADC data\n")
+
+    # for l in range(len(data)):
+    #     data_record.write(str(x[l])+","+str(data[l])+"\n")
+    # data_record.close()
 
     # Location of fundamental in the correct bin! (Helps to weed out severely distorted waveforms, misaligned data, etc.) 
     # fundamental amplitude between TBD and TBD
@@ -99,20 +124,22 @@ for n in range(5):
 
 
     if thd < -45:
-        print("THD pass")
+        print("THD pass, THD =", thd)
     else:
         print("THD fail, THD =", thd)
 
 
     if snr > 43:
-        print("SNR pass")
+        print("SNR pass, SNR =", snr)
     else:
-        print("SNR fail, SNR=", snr)
+        print("SNR fail, SNR =", snr)
 
+    print("Floor =", floor)
+    print(" dBc")
 
     record = open("fund_data.csv","a")
-    # record.write("SN, Sampling Frequency, Fundamental Amplitude, Fundamental bin location, DC component, m2k_amp, m2k_offset, SNR, THD\n")
-    record.write("B"+ sn + "_" + str(n+1)+ "," + str(my_adc.sampling_frequency) + "," + str(fund)+ "," + str(fund_bin) + "," + str(dc) + "," +  str(ampl) + "," + str(offset) + "," + str(snr)+ "," + str(thd)+ "\n")
+    # record.write("SN, Sampling Frequency, Fundamental Amplitude, Fundamental bin location, DC component, m2k_amp, m2k_offset, SNR, THD, Floor\n")
+    record.write("B"+ sn + "_" + str(n+1)+ "," + str(my_adc.sampling_frequency) + "," + str(fund)+ "," + str(fund_bin) + "," + str(dc) + "," +  str(ampl) + "," + str(offset) + "," + str(snr)+ "," + str(thd)+ "," + str(floor)+ "\n")
     record.close()
 
     # Switch in a 1000:1 attenuator, same FFT tests. THD and SNR should actually be about the same, since the ADC resolution is very high.
