6bb43f2290a86bad7529bd6849b1d30d66b878e5
Richmond <richmondeustacio@gmail.com>
2023-02-17 13:21:19 +0800

    updated path to master file

--- cn0577/eeprom_frudump.py
+++ cn0577/eeprom_frudump.py
@@ -3,7 +3,7 @@ import os
 
 def input_data(sn):
     path_EEPROM ="/sys/devices/soc0/fpga-axi@0/41620000.i2c/i2c-1/1-0050/eeprom"
-    path_masterfile = "cn0577master.bin"
+    path_masterfile = "cn0577/cn0577master.bin"
 
     os.system('fru-dump -i '+ path_masterfile + " -o " + path_EEPROM + " -s " + sn)
     # print("Succesfully loaded the FMC ID EEPROM with serial number:" + sn)
