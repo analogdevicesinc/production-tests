165022fcdc4873564b43aa84f792a86589e6a14c
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2023-03-06 11:40:59 +0200

    adrv9361: separate dcxo test from rf test
    Call the python tests for rf testing of adrv9361 and dcxo
    testing separately, ensuring they are both executed properly.
    Also added some additional sleeps for some tests and removed
    some redundant comments
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- main_tests/adrv9361_bob/rf_test.sh
+++ main_tests/adrv9361_bob/rf_test.sh
@@ -2,7 +2,19 @@ ANSWER=1
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
-echo
-python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_adrv9361_prod.py --uri="ip:analog.local" --adi-hw-map --hw=adrv9361
-ANSWER=$?
-exit $ANSWER
+source $SCRIPT_DIR/test_util.sh
+
+TEST_NAME="TEST_RF"
+
+TEST_ID="01"
+SHORT_DESC="DCXO_TESTING - Make sure the frequency counter is connected and powered."
+CMD="wait_enter ;"
+CMD+="$SCRIPT_DIR/dcxo_test.sh ;"
+run_test $TEST_ID "$SHORT_DESC" "$CMD"
+
+TEST_ID="02"
+SHORT_DESC="RF_TESTING - Make sure loopback cables are connected."
+CMD="wait_enter ;"
+CMD+="python3 -m pytest --color yes -vs $SCRIPT_DIR/../work/pyadi-iio/test/test_adrv9361_prod.py --uri=\"ip:analog.local\" --adi-hw-map --hw=adrv9361 ;"
+run_test $TEST_ID "$SHORT_DESC" "$CMD"
+
