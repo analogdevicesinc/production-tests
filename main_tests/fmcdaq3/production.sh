52f568e6e8c4b6d0e3cf1841afbcbb6dfede10c0
Ioan Dragomir <ioan.dragomir@analog.com>
2024-01-15 13:41:04 +0200

    Code consistency tweaks
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- main_tests/fmcdaq3/production.sh
+++ main_tests/fmcdaq3/production.sh
@@ -6,7 +6,7 @@ source "$SCRIPT_DIR/../lib/utils.sh"
 MODE="$1"
 case $MODE in
 	"FMCDAQ3 Test")
-		"$SCRIPT_DIR/rf_test.sh";
+		$SCRIPT_DIR/rf_test.sh
 		TEST_RESULT=$?
 		if [ $TEST_RESULT -ne 0 ]; then
 			handle_error_state "$BOARD_SERIAL"
