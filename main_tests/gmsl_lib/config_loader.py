63de4a601e7a40e634e185a40f7f5399afe3160e
Bogdan Togorean <bogdan.togorean@analog.com>
2024-02-27 13:30:37 +0200

    gmsl_lib: Implement transfer error handling
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- main_tests/gmsl_lib/config_loader.py
+++ main_tests/gmsl_lib/config_loader.py
@@ -13,21 +13,25 @@ class GMSLConfig:
             cmd_list = self.p.parse(file)
             for cmd in cmd_list:
                 chip_addr = cmd[1] // 2 # GMSL chip address
-                if cmd[0] == 'W1':
-                    self.bus.write_byte_data(chip_addr, cmd[2], cmd[3])
-                elif cmd[0] == 'W2':
-                    self.bus.write_i2c_block_data(chip_addr, cmd[2] >> 8, [cmd[2], cmd[3]])
-                elif cmd[0] == 'RMW':
-                    msg = i2c_msg.write(chip_addr, cmd[2].to_bytes(2, 'big'))
-                    self.bus.i2c_rdwr(msg)
-                    to_write = self.bus.read_byte(chip_addr)
-                    to_write &= ~cmd[4]
-                    to_write |= cmd[3]
-                    self.bus.write_i2c_block_data(chip_addr, cmd[2] >> 8, [cmd[2], to_write])
-                elif cmd[0] == 'delay':
-                    sleep(cmd[1] / 1000)
-                else:
-                    print("Unknown command!")
+                try:
+                    if cmd[0] == 'W1':
+                        self.bus.write_byte_data(chip_addr, cmd[2], cmd[3])
+                    elif cmd[0] == 'W2':
+                        self.bus.write_i2c_block_data(chip_addr, cmd[2] >> 8, [cmd[2], cmd[3]])
+                    elif cmd[0] == 'RMW':
+                        msg = i2c_msg.write(chip_addr, cmd[2].to_bytes(2, 'big'))
+                        self.bus.i2c_rdwr(msg)
+                        to_write = self.bus.read_byte(chip_addr)
+                        to_write &= ~cmd[4]
+                        to_write |= cmd[3]
+                        self.bus.write_i2c_block_data(chip_addr, cmd[2] >> 8, [cmd[2], to_write])
+                    elif cmd[0] == 'delay':
+                        sleep(cmd[1] / 1000)
+                    else:
+                        print("Unknown command!")
+                        exit()
+                except Exception as e:
+                    print("Failed I2C transfer! \nCMD:", cmd[0], "CHIP_ADDR:", hex(cmd[1]), "\nREG:", hex(cmd[2]), "\nDATA:", hex(cmd[3]))
                     exit()
                 if verify and cmd[0] != 'delay':
                     to_write = cmd[3]
